40a9b58deff89839af2203ca43109920a5970f71
PiotrekZe <piotrzerdzinski01@gamail.com>
2024-03-24 21:53:44 +0100

    add efficientnet

--- CNNEncoder.py
+++ CNNEncoder.py
@@ -2,6 +2,7 @@ import torch
 import torch.nn as nn
 from torchvision.models import resnet18, ResNet18_Weights
 from torchvision.models import convnext_small, ConvNeXt_Small_Weights
+from torchvision.models import efficientnet_b4, EfficientNet_B4_Weights
 
 
 class CNNEncoder(nn.Module):
@@ -26,6 +27,14 @@ class CNNEncoder(nn.Module):
                                                     stride=conv_layer.stride, padding=conv_layer.padding,
                                                     bias=conv_layer.bias is not None)
                 self.encode = self.convnext_encoder
+        elif self.cnn_model == 2:
+            self.cnn = efficientnet_b4(weights=EfficientNet_B4_Weights)
+            if self.num_channels == 1:
+                conv_layer = self.cnn.features[0][0]
+                self.cnn.features[0][0] = nn.Conv2d(1, conv_layer.out_channels, kernel_size=conv_layer.kernel_size,
+                                                    stride=conv_layer.stride, padding=conv_layer.padding,
+                                                    bias=conv_layer.bias is not None)
+                self.encode = self.efficientnet_encoder
 
         cnn_input_size = self.encode(torch.rand(1, self.num_channels, self.size[0], self.size[1]))
         self.linear = nn.Sequential(
@@ -47,6 +56,16 @@ class CNNEncoder(nn.Module):
     def convnext_encoder(self, x):
         x = self.cnn.features[0][0](x)
         x = self.cnn.features[0][1](x)
+        x = self.cnn.features[1](x)
+        x = self.cnn.features[2](x)
+        x = self.cnn.features[3](x)
+        x = self.cnn.features[4](x)
+        x = self.cnn.features[5](x)
+        x = x.view(x.size(0), x.size(1), -1)
+        return x
+
+    def efficientnet_encoder(self, x):
+        x = self.cnn.features[0](x)
         x = self.cnn.features[1](x)
         x = self.cnn.features[2](x)
         x = self.cnn.features[3](x)
